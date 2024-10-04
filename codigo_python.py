78b52622756988e68fd32d8c944e00658ad74346
rubences <91731294+rubences@users.noreply.github.com>
2024-10-04 19:21:08 +0000

    mas cambios

---
+++ codigo_python.py
@@ -0,0 +1,9 @@
+import math
+
+def calcular_seno(x):
+    return math.sin(x)
+
+# Ejemplo de uso
+x = 1.0  # Puedes cambiar este valor por cualquier otro
+resultado = calcular_seno(x)
+print(f"El seno de {x} es {resultado}")
