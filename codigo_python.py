06106012712f61c88c3713d32fbd73978e9b2e61
rubences <91731294+rubences@users.noreply.github.com>
2024-10-04 19:26:11 +0000

    cambios

--- codigo_python.py
+++ codigo_python.py
@@ -7,3 +7,18 @@ def calcular_seno(x):
 x = 1.0  # Puedes cambiar este valor por cualquier otro
 resultado = calcular_seno(x)
 print(f"El seno de {x} es {resultado}")
+
+
+def verificar_edad(edad):
+    if edad >= 18:
+        return "Eres mayor de edad."
+    else:
+        return "No eres mayor de edad."
+
+def main():
+    edad = int(input("Introduce tu edad: "))
+    resultado = verificar_edad(edad)
+    print(resultado)
+
+if __name__ == "__main__":
+    main()
